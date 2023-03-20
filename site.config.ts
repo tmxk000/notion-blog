e4660a1d66a8e013a0a7f59bff32ac82273a04ca
tmxk000 <106725757+tmxk000@users.noreply.github.com>
2023-03-20 20:52:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,16 +51,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: 'd8b6c7f187a34a919ab377fc82ac8a8d'
+     },
+     {
+       title: 'Contact',
+       pageId: '101d53178b124e6382049a3a350d5a96'
+     }
+   ]
 })
